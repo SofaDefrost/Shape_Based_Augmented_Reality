6470cff7c9f11ba842169136e3af6ae4382e5ac5
Tinhinane SMAIL <tinhinanesmail43@gmail.com>
2023-08-15 16:14:02 +0200

    Ajout du readme

--- README.md
+++ README.md
@@ -1,92 +1,48 @@
-# projet
+L'utilisateur doit prendre en compte les points suivants :
+
+**Important**:
+- **Pendant la capture, l'utilisateur devra appuyer sur la touche S du clavier et la touche Q pour arrêter la capture.**
+- Assurez-vous que tous les fichiers existent dans le même dossier.
+- Indiquez le chemin vers le modèle 3D ainsi que l'image.
+- Nommez le nuage de points acquis par la caméra Realsense.
+- Définissez le seuil de couleur et la phase de couleur.
+- Vérifiez le type de caméra utilisé pour pouvoir appliquer la matrice de calibration spécifique à cette caméra.
+- Pour arrêter l'exécution, il suffit de cliquer sur la touche Q.'
+
+Ce programme utilise plusieurs fonctions pour effectuer les étapes suivantes :
+
+1. **Acquisition** : Cette fonction permet de récupérer le nuage de points en connectant la caméra RealSense. Pour l'utiliser, 
+l'utilisateur doit d'abord initialiser le nom du fichier du nuage de points et le nom de l'image optique. 
+Ensuite, appelez la fonction `run_acquisition` en fournissant les paramètres suivants : 
+    `nom_fichier_nuage_points` (nom du fichier du nuage de points) et `nom_image_2D`.
+
+2. **Masquage** : Cette fonction permet de filtrer le nuage de points et de sélectionner uniquement la couleur de l'objet. Elle prend les paramètres suivants :
+
+- `nom_nuage_points` : Nom du fichier du nuage de points généré par la fonction d'acquisition.
+- `nom_nuage_points_filtre` : Nom du fichier dans lequel le nuage de points sera enregistré après l'application du masque.
+- `seuil` : Seuil utilisé pour filtrer les points en fonction de la couleur ou des coordonnées.
+- `phase_couleur` : Permet de sélectionner quelle phase des couleurs conserver, soit dans le rouge (intervalle [0-1]) et dans la colonne 0.
+
+3. **Repositionnement** : Cette fonction "repose" les points du nuage de points pour les repositionner correctement. (Remarque : La description ne mentionne pas de paramètres spécifiques pour cette fonction.)
+
+4. **Redimensionnement automatique** (resize avec 'r' en minuscule) : Cette fonction "resize" permet de redimensionner le modèle 3D et le nuage de points filtré et repositionné. L'utilisateur doit fournir les paramètres suivants :
+
+- `nom_nuage_points_repositionne` : Nom du fichier du nuage de points repositionnés au format PLY.
+- `nom_modele_3D` : Nom du fichier du modèle 3D au format PLY.
+- `nuage_points_redimensionne` : Nom du fichier dans lequel le nouveau nuage de points redimensionné sera enregistré au format PLY.
+
+ **Distance maximale** : Cette fonction "max_distance" calcule la distance maximale 
+entre le modèle 3D et le nuage de points filtré et repositionné, puis récupère cette valeur. 
 
 
+4. **Redimensionnement avec seuil** (redimensionnement avec 'R' en majuscule) :
+    - `nom_nuage_points_repositionne`
+    - Nom du fichier de sortie après le redimensionnement
+    - Facteur de redimensionnement
 
-## Getting started
-
-To make it easy for you to get started with GitLab, here's a list of recommended next steps.
-
-Already a pro? Just edit this README.md and make it your own. Want to make it easy? [Use the template at the bottom](#editing-this-readme)!
-
-## Add your files
-
-- [ ] [Create](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#create-a-file) or [upload](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#upload-a-file) files
-- [ ] [Add files using the command line](https://docs.gitlab.com/ee/gitlab-basics/add-file.html#add-a-file-using-the-command-line) or push an existing Git repository with the following command:
-
-```
-cd existing_repo
-git remote add origin https://gitlab.inria.fr/tsmail/projet.git
-git branch -M main
-git push -uf origin main
-```
-
-## Integrate with your tools
-
-- [ ] [Set up project integrations](https://gitlab.inria.fr/tsmail/projet/-/settings/integrations)
-
-## Collaborate with your team
-
-- [ ] [Invite team members and collaborators](https://docs.gitlab.com/ee/user/project/members/)
-- [ ] [Create a new merge request](https://docs.gitlab.com/ee/user/project/merge_requests/creating_merge_requests.html)
-- [ ] [Automatically close issues from merge requests](https://docs.gitlab.com/ee/user/project/issues/managing_issues.html#closing-issues-automatically)
-- [ ] [Enable merge request approvals](https://docs.gitlab.com/ee/user/project/merge_requests/approvals/)
-- [ ] [Automatically merge when pipeline succeeds](https://docs.gitlab.com/ee/user/project/merge_requests/merge_when_pipeline_succeeds.html)
-
-## Test and Deploy
-
-Use the built-in continuous integration in GitLab.
-
-- [ ] [Get started with GitLab CI/CD](https://docs.gitlab.com/ee/ci/quick_start/index.html)
-- [ ] [Analyze your code for known vulnerabilities with Static Application Security Testing(SAST)](https://docs.gitlab.com/ee/user/application_security/sast/)
-- [ ] [Deploy to Kubernetes, Amazon EC2, or Amazon ECS using Auto Deploy](https://docs.gitlab.com/ee/topics/autodevops/requirements.html)
-- [ ] [Use pull-based deployments for improved Kubernetes management](https://docs.gitlab.com/ee/user/clusters/agent/)
-- [ ] [Set up protected environments](https://docs.gitlab.com/ee/ci/environments/protected_environments.html)
-
-***
-
-# Editing this README
-
-When you're ready to make this README your own, just edit this file and use the handy template below (or feel free to structure it however you want - this is just a starting point!). Thank you to [makeareadme.com](https://www.makeareadme.com/) for this template.
-
-## Suggestions for a good README
-Every project is different, so consider which of these sections apply to yours. The sections used in the template are suggestions for most open source projects. Also keep in mind that while a README can be too long and detailed, too long is better than too short. If you think your README is too long, consider utilizing another form of documentation rather than cutting out information.
-
-## Name
-Choose a self-explaining name for your project.
-
-## Description
-Let people know what your project can do specifically. Provide context and add a link to any reference visitors might be unfamiliar with. A list of Features or a Background subsection can also be added here. If there are alternatives to your project, this is a good place to list differentiating factors.
-
-## Badges
-On some READMEs, you may see small images that convey metadata, such as whether or not all the tests are passing for the project. You can use Shields to add some to your README. Many services also have instructions for adding a badge.
-
-## Visuals
-Depending on what you are making, it can be a good idea to include screenshots or even a video (you'll frequently see GIFs rather than actual videos). Tools like ttygif can help, but check out Asciinema for a more sophisticated method.
-
-## Installation
-Within a particular ecosystem, there may be a common way of installing things, such as using Yarn, NuGet, or Homebrew. However, consider the possibility that whoever is reading your README is a novice and would like more guidance. Listing specific steps helps remove ambiguity and gets people to using your project as quickly as possible. If it only runs in a specific context like a particular programming language version or operating system or has dependencies that have to be installed manually, also add a Requirements subsection.
-
-## Usage
-Use examples liberally, and show the expected output if you can. It's helpful to have inline the smallest example of usage that you can demonstrate, while providing links to more sophisticated examples if they are too long to reasonably include in the README.
-
-## Support
-Tell people where they can go to for help. It can be any combination of an issue tracker, a chat room, an email address, etc.
-
-## Roadmap
-If you have ideas for releases in the future, it is a good idea to list them in the README.
-
-## Contributing
-State if you are open to contributions and what your requirements are for accepting them.
-
-For people who want to make changes to your project, it's helpful to have some documentation on how to get started. Perhaps there is a script that they should run or some environment variables that they need to set. Make these steps explicit. These instructions could also be useful to your future self.
-
-You can also document commands to lint the code or run tests. These steps help to ensure high code quality and reduce the likelihood that the changes inadvertently break something. Having instructions for running tests is especially helpful if it requires external setup, such as starting a Selenium server for testing in a browser.
-
-## Authors and acknowledgment
-Show your appreciation to those who have contributed to the project.
-
-## License
-For open source projects, say how it is licensed.
-
-## Project status
-If you have run out of energy or time for your project, put a note at the top of the README saying that development has slowed down or stopped completely. Someone may choose to fork your project or volunteer to step in as a maintainer or owner, allowing your project to keep going. You can also make an explicit request for maintainers.
+    
+***Remarque*** : Si l'utilisateur souhaite éviter de refaire l'acquisition à chaque fois, 
+    il peut commenter la ligne "import functions.acquisition as aq" et utiliser "aq.run_acquisition(name_pc, color_image_name)". 
+    De plus, si l'objet 3D possède ses propres couleurs spécifiques, l'utilisateur devra appeler ou décommenter l'appel à la fonction project_and_display.
+    Dans le cas contraire, il devra décommenter la ligne contenant la fonction project_and_display_without_colors.
+    """
--- main.py
+++ main.py
@@ -9,42 +9,53 @@ Created on Sun Jul 23 09:45:05 2023
 """ 
 
 
-L'utilisateur doit :
-    Important:
+L'utilisateur doit prendre en compte les points suivants :
+
+**Important**:
 - **Pendant la capture, l'utilisateur devra appuyer sur la touche S du clavier et la touche Q pour arrêter la capture.**
-- Vérifier que tous les fichiers existent dans le même dossier.
-- Charger le chemin du modèle 3D et de l'image.
-- Donner un nom au nuage de points capturé par la caméra Realsense.
-- Fixer le seuil de couleur et la phase de la couleur.
-- Vérifier le type de caméra utilisé afin d'utiliser la matrice de calibration spécifique à cette caméra.
+- Assurez-vous que tous les fichiers existent dans le même dossier.
+- Indiquez le chemin vers le modèle 3D ainsi que l'image.
+- Nommez le nuage de points acquis par la caméra Realsense.
+- Définissez le seuil de couleur et la phase de couleur.
+- Vérifiez le type de caméra utilisé pour pouvoir appliquer la matrice de calibration spécifique à cette caméra.
+- Pour arrêter l'exécution, il suffit de cliquer sur la touche Q.'
 
-Dans ce programme, il fait appel à plusieurs fonctions qui permettent de réaliser les étapes suivantes :
+Ce programme utilise plusieurs fonctions pour effectuer les étapes suivantes :
 
-1- Acquisition : Cette fonction permet de récupérer le nuage de points en connectant la caméra RealSense. Pour l'utiliser,
- l'utilisateur doit d'abord initialiser le nom du fichier du nuage de points et le nom de l'image optique. Ensuite, 
- il faut appeler la fonction `run_acquisition` en lui fournissant les paramètres suivants : `nom_fichier_nuage_points` (nom du fichier du nuage de points) et `nom_image_2D`.
+1. **Acquisition** : Cette fonction permet de récupérer le nuage de points en connectant la caméra RealSense. Pour l'utiliser, 
+l'utilisateur doit d'abord initialiser le nom du fichier du nuage de points et le nom de l'image optique. 
+Ensuite, appelez la fonction `run_acquisition` en fournissant les paramètres suivants : 
+    `nom_fichier_nuage_points` (nom du fichier du nuage de points) et `nom_image_2D`.
 
-2- Masque : Cette fonction permet de filtrer le nuage de points et de sélectionner uniquement la couleur de l'objet. Elle prend les paramètres suivants :
+2. **Masquage** : Cette fonction permet de filtrer le nuage de points et de sélectionner uniquement la couleur de l'objet. Elle prend les paramètres suivants :
 
-- `nom_nuage_points` : Le nom du fichier du nuage de points généré par la fonction d'acquisition.
-- `nom_nuage_points_filtre` : Le nom du fichier dans lequel le nuage de points sera enregistré après l'application du masque.
-- `seuil` : Le seuil utilisé pour filtrer les points en fonction de la couleur ou des coordonnées.
+- `nom_nuage_points` : Nom du fichier du nuage de points généré par la fonction d'acquisition.
+- `nom_nuage_points_filtre` : Nom du fichier dans lequel le nuage de points sera enregistré après l'application du masque.
+- `seuil` : Seuil utilisé pour filtrer les points en fonction de la couleur ou des coordonnées.
 - `phase_couleur` : Permet de sélectionner quelle phase des couleurs conserver, soit dans le rouge (intervalle [0-1]) et dans la colonne 0.
 
-3- Repositionnement : Cette fonction "repose" les points du nuage de points pour les repositionner correctement. (Remarque : La description ne mentionne pas de paramètres spécifiques pour cette fonction.)
+3. **Repositionnement** : Cette fonction "repose" les points du nuage de points pour les repositionner correctement. (Remarque : La description ne mentionne pas de paramètres spécifiques pour cette fonction.)
 
-4- Distance maximale : Cette fonction "max_distance" calcule la distance maximale entre le modèle 3D et le nuage de points filtré et repositionné, puis récupère cette valeur. (Remarque : La description ne mentionne pas de paramètres spécifiques pour cette fonction.)
+4. **Redimensionnement automatique** (resize avec 'r' en minuscule) : Cette fonction "resize" permet de redimensionner le modèle 3D et le nuage de points filtré et repositionné. L'utilisateur doit fournir les paramètres suivants :
 
-5- Redimensionnement : Cette fonction "resize" permet de redimensionner le modèle 3D et le nuage de points filtré et repositionné. L'utilisateur doit fournir les paramètres suivants :
+- `nom_nuage_points_repositionne` : Nom du fichier du nuage de points repositionnés au format PLY.
+- `nom_modele_3D` : Nom du fichier du modèle 3D au format PLY.
+- `nuage_points_redimensionne` : Nom du fichier dans lequel le nouveau nuage de points redimensionné sera enregistré au format PLY.
 
-- `nom_nuage_points_repositionnes` : Le nom du fichier du nuage de points repositionnés au format PLY.
-- `nom_modele_3D` : Le nom du fichier du modèle 3D au format PLY.
-- `nuage_points_redimensionne` : Le nom du fichier dans lequel le nouveau nuage de points redimensionné sera enregistré au format PLY.
+ **Distance maximale** : Cette fonction "max_distance" calcule la distance maximale 
+entre le modèle 3D et le nuage de points filtré et repositionné, puis récupère cette valeur. 
 
-6- ICP : L'utilisateur doit appeler la fonction `run_icp` en fournissant les paramètres suivants : 
-    le nom du fichier du nuage de points repositionnés et le modèle 3D redimensionné.
 
- 
+4. **Redimensionnement avec seuil** (redimensionnement avec 'R' en majuscule) :
+    - `nom_nuage_points_repositionne`
+    - Nom du fichier de sortie après le redimensionnement
+    - Facteur de redimensionnement
+
+    
+***Remarque*** : Si l'utilisateur souhaite éviter de refaire l'acquisition à chaque fois, 
+    il peut commenter la ligne "import functions.acquisition as aq" et utiliser "aq.run_acquisition(name_pc, color_image_name)". 
+    De plus, si l'objet 3D possède ses propres couleurs spécifiques, l'utilisateur devra appeler ou décommenter l'appel à la fonction project_and_display.
+    Dans le cas contraire, il devra décommenter la ligne contenant la fonction project_and_display_without_colors.
     """
 import numpy as np
 import cv2
@@ -54,24 +65,26 @@ import functions.icp as cp
 import functions.translation_m as tm
 import functions.repose as rp
 import functions.Resize as rz
+#import functions.resize as rzz
 #import functions.acquisition as aq
 from functions.objloader_simple import OBJ
 import functions.project_and_display as proj
 import functions.project_object_onto_image as project
-
+import functions.ply2obj as po
 
 
 #Charger le model 3D
 name_model_3D = "data_exemple/FleurDeLisThing.ply"
 
 # Récupération du nuage de points en utilisant la Realsense
-name = "data_exemple/fleur_3"
+name = "data_exemple/fleur_5"
 name_pc = name + '.ply'
 color_image_name = name + '.png'
 
 # Appeler la fonction run_acquisition pour récupérer le nuage de points
 
 #aq.run_acquisition(name_pc, color_image_name)
+
 color_image= cv2.imread(color_image_name)
 
 # Application du masque
@@ -79,29 +92,33 @@ color_image= cv2.imread(color_image_name)
 
 pc_masked_name = name + '_masked.ply'  # donner le nom pour le fichier nouveau après l'application du masque
 threshold = 0.155 # donner une valeur pour fixer un seuil de couleur
-color_phase= "bleu"
+color_phase= "blue"
 msk.mask(name_pc, pc_masked_name, threshold, color_phase)
 
 # Application de redimensionnement
 name_3D="data_exemple/model_3D"
 model_3D_resized_name =name_3D + '_resized.ply'
-facteur_echelle= 0.00099
-rz.Resize(name_model_3D, model_3D_resized_name,facteur_echelle)
+scaling_factor= 0.00099
+rz.Resize(name_model_3D, model_3D_resized_name,scaling_factor)
+# rzz.resize(model_3D_masked_name,pc_masked_name,model_3D_resized_name) # Call the function to perform automatic resizing
+
+
 
 # # Application de repositionnement
 pc_reposed_name = name + '_reposed.ply'
 translation_vector = rp.repose(pc_masked_name, pc_reposed_name)
-translation_vector[0] =-translation_vector[0]
-translation_vector[1] =translation_vector[1]
-translation_vector[2] =translation_vector[2]
+translation_vector[0] = -translation_vector[0]
+translation_vector[1] = translation_vector[1]
+translation_vector[2] = translation_vector[2]
 
 Mt = tm.translation_matrix(translation_vector)  # Matrice de translation
 Mt_t= np.transpose(Mt)
 
  # Application de l'icp  avec  plusieurs matrices de transformation et d'enregister le fichier qui a le plus petit cout 
- 
-M_icp_1, _=cp.run_icp_1(model_3D_resized_name,pc_reposed_name) 
-M_icp_2, _=cp.run_icp_2(pc_reposed_name, model_3D_resized_name)
+pc_after_multiple_icp="data_exemple/pc_after_multiple_icp.ply" 
+M_icp_1, _=cp.run_icp_1(model_3D_resized_name,pc_reposed_name,pc_after_multiple_icp) 
+print("Veuillez patienter un instant pour que l'icp_2 s'exécute.")
+M_icp_2, _=cp.run_icp_2(pc_reposed_name, pc_after_multiple_icp)
 
 
 M_icp_2_t= np.transpose(M_icp_2)
@@ -110,33 +127,44 @@ M_icp_1_t=np.transpose(M_icp_1)
 # # Matrice de calibration de la caméra realsense D415
 #M_in = np.array([[629.538, 0, 320.679, 0], [0, 629.538, 234.088, 0], [0, 0, 1, 0]])  # Matrice intrinsèque
 
-M_ex= M_icp_1 @ M_icp_2_t
+M_ex =  M_icp_1_t @ M_icp_2_t
 
-M_exx=  Mt @ M_ex 
+M_ex=  M_icp_1 @ M_icp_2
+M_ex = np.transpose(M_ex)
+
+M_exx=   Mt @ M_ex
 #Matrice de calibration de la caméra realsense D405
 M_in = np.array([[382.437, 0, 319.688, 0], [0, 382.437, 240.882, 0], [0, 0, 1, 0]])  # Matrice intrinsèquqe
 
 # Matrice de projection ==> Matrice extrinsèque transposée * Matrice intrinsèque
 Proj_1= M_in @ M_exx
+angle = np.radians(90)
 
-Projection=  Proj_1 
+# Mat_90 = np.asarray([[1, 0, 0, 0], [0, np.cos(angle), -np.sin(angle), 0], [0, np.sin(angle), np.cos(angle), 0], [0, 0, 0, 1]])
+# Projection=  Proj_1 @ Mat_90
+Projection=  Proj_1
+
+#Appel à la fonction permettant de convertir le fichier template.ply redimensionné au format .obj
+obj_file_name= name_3D +'.obj'
+po.convert_ply_to_obj(model_3D_resized_name, obj_file_name)
 # Chargement du fichier obj
 
-obj = OBJ("data_exemple/fleur_3_resized.obj", swapyz=True)
+obj = OBJ(obj_file_name, swapyz=True)
 
 # # Affichage
 h, w, _ = color_image.shape
 cv2.imshow("frame_avant", color_image)
 
-#recuperer les couleurs de model 3D
-# color_3D_Model = o3d.io.read_point_cloud("data_exemple/fleur_icp.ply")
+#recuperer les couleurs du l'objet 3D
+# color_3D_Model = o3d.io.read_point_cloud(model_3D_resized_name)
 # vertex_colors = np.asarray(color_3D_Model.colors)
 
 
 while True:
 
-     frame_apres = proj.project_and_display(color_image,obj, Projection,h,w)
-     
+     frame_apres = proj.project_and_display_without_colors(color_image,obj, Projection, h, w)
+    #Appel à la fonction permettant de projeter l'objet 3D avec ses couleurs spécifiques
+     #frame_apres = proj.project_and_display(color_image,obj, Projection, vertex_colors )
      cv2.imshow("frame_apres", frame_apres)
 
      if cv2.waitKey(1) & 0xFF == ord('q'):        
