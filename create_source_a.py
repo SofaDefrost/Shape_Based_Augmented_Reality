6fcb57725d62f0bad9f37bd32c50acf7ba6aa9c3
pchaillo <paul.chaillou@inria.fr>
2023-08-23 11:21:17 +0200

    make file working

--- create_source_a.py
+++ create_source_a.py
@@ -8,16 +8,16 @@ Created on Tue Aug 22 18:03:55 2023
 import open3d as o3d
 import numpy as np
 import copy
-import matrix_function as mf
+import functions.matrix_function as mf
 import os
 
-source = o3d.io.read_point_cloud("FleurDeLisThing.ply")
+source = o3d.io.read_point_cloud("./data_exemple/FleurDeLisThing.ply")
 
 transform_matrix = mf.create_rot_matrix_x(90)  # Assurez-vous que cette fonction existe dans matrix_function
 
 source_temp = copy.deepcopy(source)
 source_a = source_temp.transform(transform_matrix)
 
-output_filename = "source_a.ply"
+output_filename = "./data_exemple/source_a.ply"
 o3d.io.write_point_cloud(output_filename, source_a)
 
--- main_test_icp.py
+++ main_test_icp.py
@@ -6,8 +6,11 @@ Created on Wed Aug 23 10:11:49 2023
 @author: tinhinane
 """
 
-import icp as cp
-import filter_referential as fr
+import functions.icp as cp
+import functions.filter_referential as fr
 
-Mat, _=cp.run_icp_2("FleurDeLisThing.ply", "source_a.ply") 
+Mat, _=cp.run_icp_2("./data_exemple/FleurDeLisThing.ply", "./data_exemple/source_a.ply") 
+
 angles_euler= fr.angles(Mat)
+
+print(angles_euler)
